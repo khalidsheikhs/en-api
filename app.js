f589b25f70f20dec28a4f01b9996630a3ee00628
Khalid <khalidsheikhs@gmail.com>
2023-03-06 09:48:21 +0400

    code refactor

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express"),
+    cors = require("cors"),
+    PORT = process.env.PORT || 3001
 
-let app = express()
-
-let allowedOrigins = [
-    'http://localhost:3000',
-    'http://yourapp.com'
-]
-
-let password = '123';
+let app = express(),
+    allowedOrigins = [
+        'http://localhost:3000',
+        'http://yourapp.com'
+    ],
+    password = '123',
+    posts = [
+        { "id": "1", "title": "abc abc abc"},
+        { "id": "2", "title": "cde cde cde"}
+    ],
+    users = [
+        { "id": "121", "name": "khalidsheikhs", "roles": ["2001"] },
+        { "id": "122", "name": "naveedsheikhs", "roles": ["2004"] },
+        { "id": "123", "name": "farhadsheikhs", "roles": ["2003"] }
+    ]
 
 app.use(cors({
     credentials: true,
@@ -27,21 +35,10 @@ app.use(cors({
     .use(express.json())
     .use(express.urlencoded({extended:false}))
 
-const PORT = process.env.PORT || 3001
-
-let posts = [
-    { "id": "1", "title": "abc abc abc"},
-    { "id": "2", "title": "cde cde cde"}
-]
-
-let users = [
-    { "id": "121", "name": "khalidsheikhs", "roles": ["2001"] },
-    { "id": "122", "name": "naveedsheikhs", "roles": ["2004"] },
-    { "id": "123", "name": "farhadsheikhs", "roles": ["2003"] }
-]
-
-app.listen(PORT, () => {
-    console.log(`Server up and running on port ${PORT}`)
+app.post('/auth', (req, res, next) => {
+    let user = users.filter(user => (user.name === req.body.user && password === req.body.pwd))
+    // user[0]['token'] = token
+    res.json(user)
 })
 
 app.get('/users', (req, res, next) => {
@@ -49,9 +46,8 @@ app.get('/users', (req, res, next) => {
     res.json(users)
 })
 
-app.post('/auth', (req, res, next) => {
-    let user = users.filter(user => (user.name === req.body.user && password === req.body.pwd))
-    // user[0]['token'] = token
+app.get('/user', (req, res, next) => {
+    let user = users.filter(user => user.id === req.query.id)
     res.json(user)
 })
 
@@ -62,4 +58,8 @@ app.get('/posts', (req, res, next) => {
 app.get('/post', (req, res, next) => {
     let post = posts.filter(post => post.id === req.query.id)
     res.json(post)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server up and running on port ${PORT}`)
 })
